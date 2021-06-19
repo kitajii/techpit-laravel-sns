a6ba90c2d2d22be42ab2ae1c6a2c0370f645b617
kitajii <kazu.rmcf@gmail.com>
2021-06-19 15:43:10 +0900

    2-4〜2-6

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -3,19 +3,21 @@
     <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>memo</a>
 
     <ul class="navbar-nav ml-auto">
-
+        @guest
         <li class="nav-item">
-            <a class="nav-link" href="">ユーザー登録</a>
+            <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
         </li>
+        @endguest
 
+        @guest
         <li class="nav-item">
             <a class="nav-link" href="">ログイン</a>
         </li>
-
+        @endguest
         <li class="nav-item">
             <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
         </li>
-
+        @auth
         <!-- Dropdown -->
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -31,10 +33,11 @@
                 </button>
             </div>
         </li>
-        <form id="logout-button" method="POST" action="">
+        <form id="logout-button" method="POST" action="{{ route('logout') }}">
+            @scrf
         </form>
         <!-- Dropdown -->
-
+        @endauth
     </ul>
 
 </nav>
